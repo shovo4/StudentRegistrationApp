06f4d863518fc9059846dfa264e6710aaae0d8c8
Saiful Islam Shovo <58551093+shovo4@users.noreply.github.com>
2023-05-08 17:01:28 -0600

    removed default index and add bootstrap forum and saveRecord() func

--- frond-end/src/app/crud/crud.component.ts
+++ frond-end/src/app/crud/crud.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./crud.component.scss']
 })
 export class CrudComponent {
+  StudentArray : any[] = [];
 
+  name: string =""; 
+  address: string ="";
+  fee: Number =0;
+
+  currentStudentID = "";
+
+  constructor(private http: HttpClient )
+  {
+    //this.getAllStudent();
+  }
+
+  saveRecords()
+  {
+  
+    let bodyData = {
+      "name" : this.name,
+      "address" : this.address,
+      "fee" : this.fee
+    };
+    this.http.post("http://127.0.0.1:8000/student",bodyData).subscribe((resultData: any)=>
+    {
+        console.log(resultData);
+        alert("Student Registered Successfully");
+        //this.getAllStudent();
+    });
+  }
 }
