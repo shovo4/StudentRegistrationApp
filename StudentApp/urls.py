a3ee2e65a3ba0887cbc9e162890b4ad9ebaf6027
Saiful Islam Shovo <58551093+shovo4@users.noreply.github.com>
2023-05-08 18:33:14 -0600

    update method fixed and requirement.txt added

--- StudentApp/urls.py
+++ StudentApp/urls.py
@@ -5,5 +5,5 @@ from . import views
 
 urlpatterns = [
     path('students/', views.student_api, name='students'),
-    path('students/<int:id>/', views.student_api, name='student-detail'),
+    path('students/<int:id>', views.student_api, name='student-detail'),
 ]
